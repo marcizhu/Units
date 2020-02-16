c4bd07806cbfe61cb4ce79fed8a2f3aaa499d8dc
marcizhu <marc.izquierdo.hu@gmail.com>
2020-02-16 21:15:39 +0100

    Added numeric_limits for quantities

--- include/Units/extras/StdAdditions.h
+++ include/Units/extras/StdAdditions.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional> // for std::hash
+#include <limits>
 
 #include "../Units.h"
 
@@ -46,8 +47,9 @@ namespace std
 	inline Units::Quantity cbrt(Units::Quantity x) noexcept { x.root(3); return x; }
 	inline Units::Quantity pow (Units::Quantity x, int8_t exp) noexcept { return x^exp; }
 
-	inline Units::Quantity pow (Units::Quantity x, Units::Quantity y) noexcept { return pow (x,     (int8_t)y.getMagnitude()); }
-	inline Units::Quantity fmod(Units::Quantity x, Units::Quantity y) noexcept { return fmod(x.getMagnitude(), y.getMagnitude()); }
+	inline Units::Quantity pow  (Units::Quantity x, Units::Quantity y) noexcept { return pow  (x,     (int8_t)y.getMagnitude()); }
+	inline Units::Quantity fmod (Units::Quantity x, Units::Quantity y) noexcept { return fmod (x.getMagnitude(), y.getMagnitude()); }
+	inline Units::Quantity atan2(Units::Quantity x, Units::Quantity y) noexcept { return atan2(x.getMagnitude(), y.getMagnitude()); }
 
 	inline bool isinf   (Units::Quantity x) noexcept { return std::isinf   (x.getMagnitude()); }
 	inline bool isnan   (Units::Quantity x) noexcept { return std::isnan   (x.getMagnitude()); }
@@ -71,6 +73,48 @@ namespace std
 	inline Units::Quantity sinh (Units::Quantity x) noexcept { return Units::Quantity(std::sinh (x.getMagnitude())); }
 	inline Units::Quantity cosh (Units::Quantity x) noexcept { return Units::Quantity(std::cosh (x.getMagnitude())); }
 	inline Units::Quantity tanh (Units::Quantity x) noexcept { return Units::Quantity(std::tanh (x.getMagnitude())); }
-//	inline Units::Quantity log  (Units::Quantity x) noexcept { return Units::Quantity(std::log  (x.getMagnitude()), x.unit()); }
-//	inline Units::Quantity log10(Units::Quantity x) noexcept { return Units::Quantity(std::log10(x.getMagnitude()), x.unit()); }
+	inline Units::Quantity log  (Units::Quantity x) noexcept { return Units::Quantity(std::log  (x.getMagnitude()), x.getUnit()); }
+	inline Units::Quantity log10(Units::Quantity x) noexcept { return Units::Quantity(std::log10(x.getMagnitude()), x.getUnit()); }
+
+	template<>
+	class numeric_limits<Units::Quantity>
+	{
+	public:
+		static constexpr bool has_denorm_loss   = numeric_limits<double>::has_denorm_loss;
+		static constexpr bool has_infinity      = numeric_limits<double>::has_infinity;
+		static constexpr bool has_quiet_NaN     = numeric_limits<double>::has_quiet_NaN;
+		static constexpr bool has_signaling_NaN = numeric_limits<double>::has_signaling_NaN;
+		static constexpr bool is_bounded        = numeric_limits<double>::is_bounded;
+		static constexpr bool is_exact          = numeric_limits<double>::is_exact;
+		static constexpr bool is_iec559         = numeric_limits<double>::is_iec559;
+		static constexpr bool is_integer        = numeric_limits<double>::is_integer;
+		static constexpr bool is_modulo         = numeric_limits<double>::is_modulo;
+		static constexpr bool is_signed         = numeric_limits<double>::is_signed;
+		static constexpr bool is_specialized    = true;
+
+		static constexpr float_denorm_style has_denorm = numeric_limits<double>::has_denorm;
+		static constexpr float_round_style round_style = numeric_limits<double>::round_style;
+
+		static constexpr bool traps           = numeric_limits<double>::traps           || numeric_limits<float>::traps;
+		static constexpr bool tinyness_before = numeric_limits<double>::tinyness_before || numeric_limits<float>::traps;
+
+		static constexpr int digits         = numeric_limits<double>::digits;
+		static constexpr int digits10       = numeric_limits<double>::digits10;
+		static constexpr int max_digits10   = numeric_limits<double>::max_digits10;
+		static constexpr int max_exponent   = numeric_limits<double>::max_exponent;
+		static constexpr int max_exponent10 = numeric_limits<double>::max_exponent10;
+		static constexpr int min_exponent   = numeric_limits<double>::min_exponent;
+		static constexpr int min_exponent10 = numeric_limits<double>::min_exponent10;
+		static constexpr int radix          = numeric_limits<double>::radix;
+
+		static constexpr Units::Quantity denorm_min()    { return Units::Quantity(numeric_limits<double>::denorm_min()); }
+		static constexpr Units::Quantity epsilon()       { return Units::Quantity(numeric_limits<double>::epsilon()); }
+		static constexpr Units::Quantity infinity()      { return Units::Quantity(numeric_limits<double>::infinity()); }
+		static constexpr Units::Quantity lowest()        { return Units::Quantity(numeric_limits<double>::lowest()); }
+		static constexpr Units::Quantity max()           { return Units::Quantity(numeric_limits<double>::max()); }
+		static constexpr Units::Quantity min()           { return Units::Quantity(numeric_limits<double>::min()); }
+		static constexpr Units::Quantity quiet_NaN()     { return Units::Quantity(numeric_limits<double>::quiet_NaN()); }
+		static constexpr Units::Quantity round_error()   { return Units::Quantity(numeric_limits<double>::round_error()); }
+		static constexpr Units::Quantity signaling_NaN() { return Units::Quantity(numeric_limits<double>::signaling_NaN()); }
+	};
 }
